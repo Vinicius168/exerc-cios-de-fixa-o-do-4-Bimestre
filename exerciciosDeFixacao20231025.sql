08b2b21b0ea0f12d84eeaff77b46a3f25889dd09
Vinicius168 <102707503+Vinicius168@users.noreply.github.com>
2023-10-25 23:19:31 -0300

    Update exerciciosDeFixacao20231025.sql
    Exercício 2

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -6,3 +6,12 @@ for each row
 
 insert into Auditoria (mensagem)
 values ('Novo cliente');
+
+-- Exercício 2
+
+CREATE TRIGGER clienteAntesExcluir
+BEFORE DELETE ON Clientes FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão de cliente em ' + NOW());
+END;
+
